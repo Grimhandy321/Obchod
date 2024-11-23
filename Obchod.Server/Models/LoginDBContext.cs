f9b7525f8bc7aa50049eece6db665428846ffebb
Michal Příhoda <prihoda@spsejecna.cz>
2024-11-23 17:13:43 +0100

    ss

---
+++ Obchod.Server/Models/LoginDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Obchod.Server.Models
+{
+    public class LoginDBContext : DbContext
+    {
+        public LoginDBContext(DbContextOptions<LoginDBContext> options) :base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
